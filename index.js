4a4b39cba711897fc66d374960a65d3ca3962334
Luis Tenorio <luisprograming0@gmail.com>
2024-05-16 05:47:21 +0000

    chore: update dependencies and add file upload endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+require('dotenv').config();
 
 var app = express();
 
@@ -11,10 +12,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/* 
+Microservicio de metadatos de archivo
+Debes proporcionar tu propio proyecto, no la URL del ejemplo.
+Esperando:Puedes enviar un formulario que incluya una carga de archivo.
+Esperando:El campo de entrada del archivo de formulario tiene el atributo name establecido en upfile.
+Esperando:Cuando envíes un archivo, recibirás él name, type y size del archivo en bytes dentro de la respuesta JSON.
+*/
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
